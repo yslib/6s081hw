9d06bee5298d747dc302cc3706dcc52bab77f686
Shuoliu Yang <visysl@outlook.com>
2021-03-26 14:24:31 +0800

    thread finished

--- notxv6/ph.c
+++ notxv6/ph.c
@@ -8,6 +8,8 @@
 #define NBUCKET 5
 #define NKEYS 100000
 
+static pthread_mutex_t locks[NBUCKET];
+
 struct entry {
   int key;
   int value;
@@ -39,6 +41,7 @@ static
 void put(int key, int value)
 {
   int i = key % NBUCKET;
+  pthread_mutex_lock(&locks[i]);
 
   // is the key already present?
   struct entry *e = 0;
@@ -53,6 +56,8 @@ void put(int key, int value)
     // the new is new.
     insert(key, value, &table[i], table[i]);
   }
+
+  pthread_mutex_unlock(&locks[i]);
 }
 
 static struct entry*
@@ -113,6 +118,11 @@ main(int argc, char *argv[])
   assert(NKEYS % nthread == 0);
   for (int i = 0; i < NKEYS; i++) {
     keys[i] = random();
+  }
+
+  // init lock
+  for(int i =0;i<NBUCKET;i++){
+    pthread_mutex_init(&locks[i],NULL);
   }
 
   //
